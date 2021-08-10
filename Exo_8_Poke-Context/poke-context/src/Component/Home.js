d124ec5052a9c14eabf6bf37ef7fbb547859a292
Eliott17 <poulaineliott@gmail.com>
2021-08-10 16:14:18 +0200

    add loader gif

--- Exo_8_Poke-Context/poke-context/src/Component/Home.js
+++ Exo_8_Poke-Context/poke-context/src/Component/Home.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import hyperBall from "../Image/HyperBall.png";
 
 // CSS
 import "./Home.css";
@@ -10,6 +11,7 @@ const Home = () => {
     const [pokemon, setPokemon] = useState(null);
     const [ID, setID] = useState(1);
     const [picture, setPicture] = useState(1);
+    const [isLoading, setIsLoading] = useState(true);
 
 
     useEffect(() => {
@@ -19,24 +21,29 @@ const Home = () => {
 
                 // console.log(res);
                 setPokemon(res);
+                setIsLoading(false);
             })
     }, [ID])
-    // console.log(pokemon)
 
-    if (!pokemon) {
-        return (null)
-    }
 
     function randomNumber() {
+
         setPicture(Math.floor(Math.random() * 10) + 1)
-        setID(Math.floor(Math.random() * 898) + 1)
+        setID(Math.floor(Math.random() * 649) + 1)
     }
-    console.log(`${ID} is id`)
+    // console.log(`${ID} is id`)
+    // console.log(picture)
 
     // function isShiny() {
     //     return setPicture(Math.floor(Math.random() * 10) + 1)
     // }
-    console.log(picture)
+    if (isLoading) {
+        return (
+            <div className="loader">
+                <img className="loader-image" src={hyperBall} alt="Hyper ball " />
+            </div>
+        )
+    }
 
     return (
         <div className="home-div">
@@ -44,15 +51,15 @@ const Home = () => {
                 <button onClick={randomNumber} >Get a Pokemon</button>
                 <div>
                     <p>Pokemon's name: {pokemon.forms[0].name}</p>
-                    <p>Pokemon's height: {pokemon.height}</p>
-                    <p>Pokemon's weight: {pokemon.weight}</p>
+                    <p>Pokemon's height: {pokemon.height / 10} m</p>
+                    <p>Pokemon's weight: {pokemon.weight / 10} kg</p>
                     <p>Pokemon's type: {(pokemon.types.length === 2)
                         ? `${pokemon.types[0].type.name} and  ${pokemon.types[1].type.name}`
                         : `${pokemon.types[0].type.name}`}</p>
                     <div className="image-div">
                         {(picture === 10)
-                            ? <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${ID}.png`} alt={`${pokemon.forms[0].name}`} />
-                            : <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ID}.png`} alt={`${pokemon.forms[0].name}`} />
+                            ? <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${ID}.gif`} alt={`${pokemon.forms[0].name}`} />
+                            : <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${ID}.gif`} alt={`${pokemon.forms[0].name}`} />
                         }
                     </div>
                     {(picture === 10)
