839b5e2ac11b849cf2b42b735b6697586bb89a2a
Eliott17 <poulaineliott@gmail.com>
2021-08-10 12:01:23 +0200

    image, style, shyni  add

--- Exo_8_Poke-Context/poke-context/src/Component/Home.js
+++ Exo_8_Poke-Context/poke-context/src/Component/Home.js
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react";
 
+// CSS
+import "./Home.css";
+
 const Home = () => {
 
 
 
     const [pokemon, setPokemon] = useState(null);
     const [ID, setID] = useState(1);
+    const [picture, setPicture] = useState(1);
 
 
     useEffect(() => {
@@ -24,20 +28,40 @@ const Home = () => {
     }
 
     function randomNumber() {
-        return setID(Math.floor(Math.random() * 100) + 1)
+        setPicture(Math.floor(Math.random() * 10) + 1)
+        setID(Math.floor(Math.random() * 898) + 1)
     }
+    console.log(`${ID} is id`)
+
+    // function isShiny() {
+    //     return setPicture(Math.floor(Math.random() * 10) + 1)
+    // }
+    console.log(picture)
 
     return (
-        <div>
-            <h1>Home</h1>
-            <button onClick={randomNumber} >Get a Pokemon</button>
-            <p>Pokemon's name: {pokemon.forms[0].name}</p>
-            <p>Pokemon's height: {pokemon.height}</p>
-            <p>Pokemon's weight: {pokemon.weight}</p>
-            <p>Pokemon's type: {(pokemon.types.length === 2)
-                ? `${pokemon.types[0].type.name} and  ${pokemon.types[1].type.name}`
-                : `${pokemon.types[0].type.name}`}</p>
-        </div>
+        <div className="home-div">
+            <div className="content-div">
+                <button onClick={randomNumber} >Get a Pokemon</button>
+                <div>
+                    <p>Pokemon's name: {pokemon.forms[0].name}</p>
+                    <p>Pokemon's height: {pokemon.height}</p>
+                    <p>Pokemon's weight: {pokemon.weight}</p>
+                    <p>Pokemon's type: {(pokemon.types.length === 2)
+                        ? `${pokemon.types[0].type.name} and  ${pokemon.types[1].type.name}`
+                        : `${pokemon.types[0].type.name}`}</p>
+                    <div className="image-div">
+                        {(picture === 10)
+                            ? <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${ID}.png`} alt={`${pokemon.forms[0].name}`} />
+                            : <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ID}.png`} alt={`${pokemon.forms[0].name}`} />
+                        }
+                    </div>
+                    {(picture === 10)
+                        ? <p>Damn he's shiny, don't forget to catch it !</p>
+                        : null
+                    }
+                </div>
+            </div>
+        </div >
     )
 }
 
--- Exo_8_Poke-Context/poke-context/src/Component/Login.js
+++ Exo_8_Poke-Context/poke-context/src/Component/Login.js
@@ -1,5 +1,8 @@
 import { useForm } from "react-hook-form";
 
+// CSS 
+import "./Login.css"
+
 
 const Login = () => {
 
@@ -9,13 +12,15 @@ const Login = () => {
 
     return (
         <div>
-            <h1>Login</h1>
-
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input placeholder="Username" {...register("username", { required: true })} maxLength={15} />
-                {errors.username && <span>Give us a username please</span>}
-                <input placeholder="Password" {...register("password", { required: true })} minLength={7} />
-                {errors.password && <span>The password should be longer than 7 characters</span>}
+                <div className="input-div">
+                    <input className="form-input" placeholder="Username" {...register("username", { required: true })} maxLength={15} />
+                    <input className="form-input" placeholder="Password" {...register("password", { required: true })} minLength={7} />
+                </div>
+                <div className="errors-div">
+                    {errors.username && <span>Give us a username please</span>}
+                    {errors.password && <span>The password should be longer than 7 characters</span>}
+                </div>
                 <input type="submit" />
             </form>
         </div>
